5ec64ad4eedba99353c5383b03a4c96dfad85301
SamuelDavidRuedaSuarezADSO <samueldavidruedasuarez3@gmail.com>
2024-06-19 16:19:43 -0500

    holaXD

--- recorrer/app.js
+++ recorrer/app.js
@@ -57,4 +57,53 @@ const p = dom.getElementById("api")
 
 p.innerText = "¿QUE ES UN API?"
 
-console.log(body.nextElementSibling)
+// console.log(body.nextElementSibling)
+
+const cards = dom.getElementById("cards")
+
+const cardHijo = cards.children
+
+const array = [...cardHijo]
+
+array.map((x, i) => {
+  if (i % 2 == 0) {
+    x.classList.toggle('bg-white')
+    const f = x.children
+    let y = f[1]
+    y = y.children
+    let w = [...y]
+    w.map((k, l) => {
+      if (l == 1) {
+        k.classList.toggle('color-black')
+      }
+    })
+  }
+})
+
+const $formulario = dom.getElementById('search')
+$formulario.setAttribute("enctype", "multipart/form-data")
+
+const $input = dom.querySelector('input')
+$input.setAttribute("autocomplete", "off")
+$input.removeAttribute("placeholder")
+$input.setAttribute("placeholder", "señor...")
+console.log($input)
+
+console.clear()
+
+// custom propertis css
+
+
+const $html = dom.documentElement
+
+const mineshatf = getComputedStyle($html).getPropertyValue("--hevymetal")
+const $white = getComputedStyle($html).getPropertyValue("--white")
+
+$html.style.setProperty("--hevymetal", "#fff")
+$html.style.setProperty("--white", "#000")
+
+console.log(mineshatf, $white)
+
+const $parrafo = dom.querySelector('#elemento')
+$parrafo.textContent = `Hola <b>soy</b> anticrito69`
+// console.log($parrafo)
